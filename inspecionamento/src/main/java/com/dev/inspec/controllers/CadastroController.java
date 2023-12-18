bebc4980ef3fcb746a9ace6f398de79de26f8747
wellersonjesus <wellersonjesus@hotmail.com>
2023-12-18 06:21:55 -0300

    Metodos autalizar e deletar CadastroDTO

--- inspecionamento/src/main/java/com/dev/inspec/controllers/CadastroController.java
+++ inspecionamento/src/main/java/com/dev/inspec/controllers/CadastroController.java
@@ -4,12 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.dev.inspec.dto.CadastroDTO;
 import com.dev.inspec.entities.Cadastro;
@@ -19,32 +14,54 @@ import com.dev.inspec.repositories.CadastroRepository;
 @RequestMapping(value = "/cadastros")
 public class CadastroController {
 
-    @Autowired
-    private CadastroRepository repository;
+	@Autowired
+	private CadastroRepository repository;
 
-    @GetMapping
-    public List<CadastroDTO> findAll() {
-        List<Cadastro> cadastros = repository.findAll();
-        return cadastros.stream().map(this::toDTO).collect(Collectors.toList());
-    }
+	@GetMapping
+	public List<CadastroDTO> findAll() {
+		List<Cadastro> cadastros = repository.findAll();
+		return cadastros.stream().map(this::toDTO).collect(Collectors.toList());
+	}
 
-    @GetMapping(value = "/{id}")
-    public CadastroDTO findById(@PathVariable Long id) {
-        Cadastro cadastro = repository.findById(id).orElse(null);
-        if (cadastro != null) {
-            return toDTO(cadastro);
-        } else {
-            return null;
-        }
-    }
+	@GetMapping(value = "/{id}")
+	public CadastroDTO findById(@PathVariable Long id) {
+		Cadastro cadastro = repository.findById(id).orElse(null);
+		if (cadastro != null) {
+			return toDTO(cadastro);
+		} else {
+			return null;
+		}
+	}
 
-    @PostMapping
-    public CadastroDTO insert(@RequestBody Cadastro cadastro) {
-        Cadastro savedCadastro = repository.save(cadastro);
-        return toDTO(savedCadastro);
-    }
+	@PostMapping
+	public CadastroDTO insert(@RequestBody Cadastro cadastro) {
+		Cadastro savedCadastro = repository.save(cadastro);
+		return toDTO(savedCadastro);
+	}
 
-    private CadastroDTO toDTO(Cadastro cadastro) {
-        return new CadastroDTO(cadastro.getId(), cadastro.getContato());
-    }
+	@PutMapping(value = "/{id}")
+	public CadastroDTO update(@PathVariable Long id, @RequestBody Cadastro cadastroAtualizado) {
+		Cadastro existingCadastro = repository.findById(id).orElse(null);
+
+		if (existingCadastro != null) {
+			existingCadastro.setContato(cadastroAtualizado.getContato());
+			existingCadastro.setCpfoucnpj(cadastroAtualizado.getCpfoucnpj());
+			existingCadastro.setEndereco(cadastroAtualizado.getEndereco());
+
+			Cadastro updatedCadastro = repository.save(existingCadastro);
+
+			return toDTO(updatedCadastro);
+		} else {
+			return null;
+		}
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public void delete(@PathVariable Long id) {
+		repository.deleteById(id);
+	}
+
+	private CadastroDTO toDTO(Cadastro cadastro) {
+		return new CadastroDTO(cadastro.getId(), cadastro.getContato());
+	}
 }

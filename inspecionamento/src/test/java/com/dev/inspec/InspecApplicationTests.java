c3daf70616ccd3359e0ead66dea6cd458a40789b
wellersonjesus <wellersonjesus@hotmail.com>
2024-03-14 20:01:39 -0300

    ajustes prod ok ...

--- inspecionamento/src/test/java/com/dev/inspec/InspecApplicationTests.java
+++ inspecionamento/src/test/java/com/dev/inspec/InspecApplicationTests.java
@@ -1,13 +1,13 @@
-package com.dev.inspec;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class InspecApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.dev.inspec;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class InspecApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
